79295280f9401c42d9d3b2c0e57d3441b3a8c243
marquis-codes <abahmarquis@gmail.com>
2026-02-14 01:13:23 +0100

    updatd

---
+++ .output/public/_nuxt/Bn2WUFsW.js
@@ -0,0 +1,1 @@
+import{p as t}from"./_ZNUhKWF.js";import{r as a}from"./D1Gmuvwg.js";import{X as i,_ as o}from"./DjK2_PfT.js";import"./5YKSnjij.js";import"./D9r6UuLH.js";import"./DxURHT3k.js";import"./BNGpIwps.js";const v=i({defaults:{"@type":"Review"},inheritMeta:["inLanguage"],resolve(r,e){return r.reviewRating=o(r.reviewRating,e,a),r.author=o(r.author,e,t),r}});export{v as reviewResolver};
---
+++ .output/public/_nuxt/DzUWAGhZ.js
@@ -0,0 +1,1 @@
+import{X as e}from"./DjK2_PfT.js";const t=e({defaults:{"@type":"AggregateRating"}});export{t as aggregateRatingResolver};
